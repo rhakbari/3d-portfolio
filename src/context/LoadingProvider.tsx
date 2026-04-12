04d26c65376ae3def68df96bfeb875327081bc5f
rhakbari <rhakbari.96@gmail.com>
2026-04-12 23:19:30 +0500

    build fixes

--- src/context/LoadingProvider.tsx
+++ src/context/LoadingProvider.tsx
@@ -1,5 +1,23 @@
-import { PropsWithChildren } from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+interface LoadingContextType {
+  isLoading: boolean;
+  setIsLoading: (value: boolean) => void;
+}
+
+const LoadingContext = createContext<LoadingContextType>({
+  isLoading: true,
+  setIsLoading: () => {},
+});
+
+export const useLoading = () => useContext(LoadingContext);
 
 export const LoadingProvider = ({ children }: PropsWithChildren) => {
-  return <main className="main-body">{children}</main>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      <main className="main-body">{children}</main>
+    </LoadingContext.Provider>
+  );
 };
